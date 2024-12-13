6852110133bb9cf261c7d8751e7c779844263df0
rullyvarelio <rully.v6002gmail.com>
2024-12-13 22:50:53 +0700

    add signout btn

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,11 +1,16 @@
 import UserProfile from "@/components/user-profile";
 import Tasks from "@/components/tasks";
+import { signOutAction } from "@/actions/auth-action";
+import { Button } from "@/components/ui/button";
 
 export default function Loggedin() {
   return (
     <main>
       <UserProfile />
       <Tasks />
+      <form action={signOutAction}>
+        <Button>Sign Out</Button>
+      </form>
     </main>
   );
 }
